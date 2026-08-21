685f3a9071f46c8d0fd8773b51153433132f5c7b
Airo CaeMDe <annabellepork@proton.me>
2026-08-22 00:41:27 +0300

    Update profiles.js

--- data/profiles.js
+++ data/profiles.js
@@ -1,27 +1,10 @@
-const PROFILES = {
-
-  copy_pasta: {
-    username: "copy_pasta",
-    displayName: "copy_pasta",
-
-    avatar:
-      "https://blogger.googleusercontent.com/img/a/AVvXsEiNU5aLNwKBZvNgLsvxfKYA0hduyABhvfQrHryVIrw1Q2WVFne8Aop849IRmthJlGuRphRDtjX_d-pvH8gcUtCoAL-zkhXAP0utpiVidiXL57SIMaMBjeaXnGhTZ_j2wQj1RZxI-V5TsjxtARZSpoxZ8UGuDX_9t9hmiL6k1OyP6ilE8WmYwfKmHTQFvFg",
-
-    description: "ліньки розписувати",
-
-    status: {
-      type: "online",
-      lastSeen: null
-    },
-
-    family: {
-      name: "стусни з dreaming_romance",
-      url: "/archat/profile.html?user=dreaming_romance"
-    },
-
-    postsFile: "copy_pasta",
-
-    postsCount: 25
-  }
-
+const profiles = {
+    copy_pasta: {
+        username: "copy_pasta",
+        displayName: "copy_pasta",
+        avatar: "https://aircmd.github.io/archat/assets/avatars/copy_pasta.jpg",
+        status: "online",
+        postsCount: 25,
+        description: "Основний тестовий акаунт ARChat."
+    }
 };
